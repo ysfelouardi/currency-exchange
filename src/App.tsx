bacffeca19efc57120f9aaea6526fa74ac6e0fdd
ysfelouardi <youssef.94.elouardi@gmail.com>
2021-11-13 18:14:31 +0100

    feat : adding react router dom and impl the nav logic in the navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,28 @@ import Theme from "./theme";
 import Layout from "./components/Layout";
 import ConverterFeature from "./features/ConverterFeature";
 import ExchangeHistory from "./features/ExchangeHistory";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { memo } from "react";
+
+const ConverterPage = memo(() => (
+  <>
+    <ConverterFeature />
+    <hr />
+    <ExchangeHistory />
+  </>
+));
 
 function App() {
   return (
     <Theme>
-      <Layout>
-        <ConverterFeature />
-        <hr />
-        <ExchangeHistory />
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path={"/"} element={<ConverterPage />} />
+            <Route path={"/history"} element={<h1>History Page</h1>} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </Theme>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,38 @@
-import { memo } from "react";
+import { memo, useCallback, useMemo } from "react";
 import { Logo, StyledHeader } from "./styles";
 import { ReactComponent as Icon } from "../../assets/svgs/find_replace_black_24dp.svg";
 import NavBar from "../NavBar";
+import { useNavigate, useLocation } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const defaultLink = useMemo(() => {
+    if (pathname === "/") {
+      return "currencyConverter";
+    } else if (pathname === "/history") {
+      return "currencyHistory";
+    }
+  }, [pathname]);
+
+  const handleNavigation = useCallback(
+    (item: string) => {
+      if (item === "currencyConverter") {
+        navigate("/");
+      } else if (item === "currencyHistory") {
+        navigate("/history");
+      }
+    },
+    [navigate]
+  );
+
   return (
     <StyledHeader>
       <Logo>
         <Icon /> Currency <strong>Exchange</strong>
       </Logo>
-      <NavBar>
+      <NavBar onItemChange={handleNavigation} defaultActiveItem={defaultLink}>
         <NavBar.Item title={"currencyConverter"}>
           currency converter
         </NavBar.Item>
